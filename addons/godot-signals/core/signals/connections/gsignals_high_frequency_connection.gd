87d130c27cc0d61e4b1f048f4c5732a79b524da1
Adrien Quillet <adrien.quillet@icloud.com>
2025-03-30 18:16:11 +0200

    Refactor GSignals high-frequency connection handling
    * Remove unused variable declaration in _handle_signal function.
    * Add result variable declaration for better clarity in signal processing.
    * Clean up commented-out debounce, throttle, and buffer operations to streamline the code.
    * Ensure callback invocation logic remains intact for improved signal handling.

--- addons/godot-signals/core/signals/connections/gsignals_high_frequency_connection.gd
+++ addons/godot-signals/core/signals/connections/gsignals_high_frequency_connection.gd
@@ -117,7 +117,6 @@ var _callback: Callable
 %s
 
 func _handle_signal(%s) -> void:
-    var result
 %s
 """
     # Generate argument list
@@ -149,7 +148,7 @@ func _handle_signal(%s) -> void:
             operation_calls += "    if not _filter_%d.call(%s):\n        return\n" % [filter_count, current_args]
             filter_count += 1
         elif operation is GSignalsMapOperation:
-            private_vars += "var _map_%d: Callable\n" % map_count
+            private_vars += "var _map_%d: Callable\nvar result:Variant\n" % map_count
             operation_calls += "    result = _map_%d.call(%s)\n" % [map_count, current_args]
             current_args = "result"
             has_mapper = true
@@ -164,21 +163,6 @@ func _handle_signal(%s) -> void:
             debounce_count += 1
         else:
             push_error("Unknown operation type: %s" % operation.type)
-
-
-        # "debounce":
-        #     private_vars += "var _debounce_%d: float\n" % debounce_count
-        #     operation_calls += "    _pending_args = %s\n    _timer = _debounce_%d\n    return\n" % [current_args, debounce_count]
-        #     debounce_count += 1
-        # "throttle":
-        #     private_vars += "var _throttle_%d: float\n" % throttle_count
-        #     operation_calls += "    var current_time = Time.get_ticks_msec()\n    if current_time - _last_time < _throttle_%d:\n        return\n    _last_time = current_time\n" % throttle_count
-        #     throttle_count += 1
-        # "buffer":
-        #     private_vars += "var _buffer_%d: int\n" % buffer_count
-        #     operation_calls += "    _buffer.append(%s)\n    if _buffer.size() < _buffer_%d:\n        return\n    result = _buffer.duplicate()\n    _buffer.clear()\n" % [current_args, buffer_count]
-        #     current_args = "result"
-        #     buffer_count += 1
 
     # Add callback call based on whether we have a mapper or not
     if has_mapper:
